3d6e4c0356c5571e6bf458ee9646da6524245dad
Prajeesh MP <prajeesh@splendre.com>
2022-11-23 09:12:29 +0530

    Update: Remove old pages

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    arrowParens: "always",
+    bracketSameLine: false,
+    bracketSpacing: true,
+    embeddedLanguageFormatting: "auto",
+    htmlWhitespaceSensitivity: "css",
+    insertPragma: false,
+    jsxSingleQuote: false,
+    printWidth: 150,
+    proseWrap: "preserve",
+    quoteProps: "as-needed",
+    requirePragma: false,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    trailingComma: "none",
+    useTabs: false,
+    vueIndentScriptAndStyle: false
+};
